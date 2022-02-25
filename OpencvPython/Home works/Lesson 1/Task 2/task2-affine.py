e905e458f2400321ed8cc8f233dbad500ff2849d
Shacall <davleev_zinnur@mail.ru>
2022-02-26 00:34:40 +0300

    add affine warp

---
+++ OpencvPython/Home works/Lesson 1/Task 2/task2-affine.py
@@ -0,0 +1,118 @@
+import math
+import cv2
+import numpy as np
+
+img = cv2.imread("../../../Resources/cards.jpg")
+
+mousePosition: tuple[int, int] = (0, 0)
+isMouseDrag: bool = False
+isMouseDown: bool = False
+rectStartPos: tuple[int, int] = (0, 0)
+groups: list[tuple[int, int]] = [()]
+selectedPointIndex: int = -1
+selectedGroupIndex: int = 0
+
+
+def distance(a, b):
+    return int(math.sqrt((a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1])))
+
+
+def onMouse(event, x, y, flags, param):
+    global mousePosition
+    global isMouseDrag
+    global isMouseDown
+    global rectStartPos
+    global groups
+    global selectedPointIndex
+    global selectedGroupIndex
+
+    mousePosition = (x, y)
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        rectStartPos = (x, y)
+        isMouseDrag = True
+
+    if event == cv2.EVENT_LBUTTONUP:
+        groups[selectedGroupIndex] = [rectStartPos, (x, rectStartPos[1]), (x, y)]
+        isMouseDrag = False
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        for i, point in enumerate(groups[selectedGroupIndex]):
+            if distance(point, (x, y)) < 20:
+                selectedPointIndex = i
+        isMouseDown = True
+
+    if event == cv2.EVENT_RBUTTONUP:
+        selectedPointIndex = -1
+        isMouseDown = False
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        if isMouseDown and selectedPointIndex != -1:
+            groups[selectedGroupIndex][selectedPointIndex] = (x, y)
+
+
+cv2.imshow("Image", img)
+cv2.setMouseCallback("Image", onMouse)
+
+while True:
+    img = cv2.imread("../../../Resources/cards.jpg")
+    img_copy = img.copy()
+
+    for i, points in enumerate(groups):
+        vertexColor = (0, 0, 255)
+        lineColor = (0, 0, 255)
+        if i == selectedGroupIndex:
+            vertexColor = (255, 0, 0)
+            lineColor = (0, 255, 0)
+
+        if isMouseDrag and i == selectedGroupIndex:
+            cv2.rectangle(img_copy, rectStartPos, (mousePosition[0], mousePosition[1]), (0, 255, 0), 2)
+        else:
+            if points:
+                width = distance(points[0], points[1])
+                height = distance(points[1], points[2])
+
+                # Calculate affine matrix
+                fromMatrix = np.array([points[0], points[1], points[2]]).astype(np.float32)
+                toMatrix = np.array([
+                    [0, 0],
+                    [width, 0],
+                    [width, height],
+                ]).astype(np.float32)
+                matrix = cv2.getAffineTransform(fromMatrix, toMatrix)
+
+                # Warp affine image
+                out_image = cv2.warpAffine(img, matrix, (width, height))
+                cv2.imshow(f"Warped image {i}", out_image)
+
+                # Draw frame lines
+                cv2.line(img_copy, points[0], points[1], lineColor, 2)
+                cv2.line(img_copy, points[1], points[2], lineColor, 2)
+
+                oppositePoint = (points[0][0] + points[2][0] - points[1][0], points[0][1] + points[2][1] - points[1][1])
+
+                cv2.line(img_copy, points[2], oppositePoint, lineColor, 2)
+                cv2.line(img_copy, oppositePoint, points[0], lineColor, 2)
+
+                # Draw frame vertices
+                for point in points:
+                    cv2.circle(img_copy, point, 5, vertexColor, -1)
+
+    cv2.putText(img_copy, f"Selected group: {selectedGroupIndex}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0),
+                2)
+
+    cv2.imshow("Image", img_copy)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+    if key == ord('c'):
+        print("Clear points")
+        groups[selectedGroupIndex] = []
+    if key == ord('d'):
+        selectedGroupIndex += 1
+        if len(groups) <= selectedGroupIndex:
+            groups.append(())
+    if key == ord('a'):
+        selectedGroupIndex -= 1
+        if selectedGroupIndex < 0:
+            selectedGroupIndex = 0
--- OpencvPython/Home works/Lesson 2/test.py
+++ OpencvPython/Home works/Lesson 2/test.py
@@ -1,4 +1,7 @@
+import math
+
 import cv2
+import time
 
 img = cv2.imread("../../Resources/lena.png")
 img = cv2.resize(img, None, fx=0.5, fy=0.5)
@@ -6,10 +9,23 @@ imgSize = (img.shape[1], img.shape[0])
 
 center = (imgSize[0] / 2, imgSize[1] / 2)
 
-rotateMatrix = cv2.getRotationMatrix2D(center, 45, 1)
+pTime = 0
 
-rotatedImg = cv2.warpAffine(img, rotateMatrix, imgSize)
+while True:
+    img = cv2.imread("../../Resources/lena.png")
+    img = cv2.resize(img, None, fx=0.5, fy=0.5)
 
-cv2.imshow("Image", img)
-cv2.imshow("Rotated Image", rotatedImg)
-cv2.waitKey()
+    cTime = time.time()
+    fps = int(1 / (cTime - pTime))
+    pTime = cTime
+
+    rotateMatrix = cv2.getRotationMatrix2D(center, time.time() * 90, 1/math.sqrt(2))
+    rotatedImg = cv2.warpAffine(img, rotateMatrix, imgSize)
+
+    cv2.putText(rotatedImg, f"{fps}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    cv2.imshow("Rotated Image", rotatedImg)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
